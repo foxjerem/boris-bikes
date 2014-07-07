51c2bc760732ab8e86018961f3b599c4d740a716
Joe Dowdell <joedowdell@gmail.com>
2014-07-07 16:49:17 +0100

    Bike class added to Boris bikes

---
+++ lib/bike.rb
@@ -0,0 +1,35 @@
+class Bike
+
+	def initialize
+		@broken = false
+		@docked = false
+	end
+
+	def broken?
+		@broken
+	end
+
+	def break!
+		@broken = true
+		self
+	end
+
+	def fix!
+		@broken = false
+		self
+	end
+
+	def docked?
+		@docked
+	end
+
+	def dock!
+		@docked = true
+		self
+	end
+	
+	def undock!
+		@docked = false
+		self
+	end
+end
---
+++ spec/bike_spec.rb
@@ -0,0 +1,43 @@
+require 'bike'
+
+describe Bike do
+
+	let(:bike) {Bike.new}
+
+	context 'state of repair' do
+	
+		it 'should not be broken initially' do
+			expect(bike.broken?).to eq false 
+		end
+
+		it 'can be broken' do
+			bike.break!
+			expect(bike.broken?).to be true
+		end
+
+		it 'can be fixed' do
+			bike.break!.fix!
+			expect(bike.broken?).to be false
+		end
+	
+	end
+
+	context 'docking' do
+
+		it 'it should not be docked initially' do
+			expect(bike.docked?).to be false
+		end
+
+		it 'should know if it is docked' do
+			bike.dock!		
+			expect(bike.docked?).to be true
+		end
+
+		it 'should know if it is undocked' do
+			bike.dock!.undock!		
+			expect(bike.docked?).to be false
+		end
+
+	end
+
+end
