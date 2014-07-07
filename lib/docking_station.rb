90e647a27beda118d72048bd9d7788b583b177a1
Joe Dowdell <joedowdell@gmail.com>
2014-07-07 18:25:16 +0100

    Added error release methods to Docking class

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -17,11 +17,18 @@ class DockingStation
 	end
 
 	def release(bike)
+		raise "Argument is not a bike" unless bike.is_a?(Bike)
+		raise "Station is empty" if self.empty?
+		raise "Bike is not in the station" unless @bikes.include?(bike)
 		@bikes.delete(bike)
 	end
 
 	def full?
 		bike_count == @capacity
+	end
+
+	def empty?
+		bike_count == 0
 	end
 
 	def available_bikes
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -16,21 +16,21 @@ describe DockingStation do
 		expect(station.bike_count).to eq 1
 	end
 
-	it 'should release a bike' do
-		station.dock(bike)
-		station.release(bike)
-		expect(station.bike_count).to eq 0
-	end
-
 	it 'should know when it is full' do
 		expect(station).not_to be_full
 		fill_station	
 		expect(station).to be_full
 	end
 
+	it 'should know when it is empty' do
+		expect(station).to be_empty
+		fill_station	
+		expect(station).not_to be_empty
+	end
+
 	it 'should not accept a bike if full' do
 		fill_station
-		expect(lambda { station.dock(bike)}).to raise_error(RuntimeError)
+		expect{ station.dock(bike)}.to raise_error(RuntimeError)
 	end
 
 	it 'should provide a list of available bikes' do
@@ -39,6 +39,31 @@ describe DockingStation do
 		station.dock(working_bike)
 		station.dock(broken_bike)
 		expect(station.available_bikes).to eq([working_bike])
+	end
+
+	context 'release' do
+
+		it 'should release a bike' do
+			station.dock(bike)
+			station.release(bike)
+			expect(station.bike_count).to eq 0
+		end
+
+		it 'should raise an error if the argument passed is not a bike' do
+			station.dock(bike)
+			expect{station.release("grabncbjh")}.to raise_error(RuntimeError)
+		end
+
+		it 'should raise error if bike is not in the station' do
+			bike2 = Bike.new
+			station.dock(bike2)
+			expect{station.release(bike)}.to raise_error(RuntimeError)
+		end
+
+		it 'should raise error if the station is empty' do
+			expect{station.release(bike)}.to raise_error(RuntimeError)
+		end
+
 	end
 
 end
